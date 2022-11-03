9c1dd28cec530b83265bdcc0afdb83a59b9515eb
polidex <lp.igor@gmail.com>
2022-11-03 18:56:40 -0300

    feat: reqt 09 incompleto

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -4,8 +4,7 @@ import SimpleFighter from './SimpleFighter';
 interface Fighter extends SimpleFighter {
   defense: number;
   energy?: Energy;
-  attack(enemy: Fighter): void;
-  special?(enemy: Fighter): void;
+  special?(enemy: SimpleFighter): void;
   levelUp(): void;
 }
 
