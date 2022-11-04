b579b3d41863a1748fb0d5539442c60c2ae5e446
polidex <lp.igor@gmail.com>
2022-11-04 11:27:12 -0300

    feat: reqt 10 done

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,7 +1,24 @@
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
-class PVP implements Battle {
+class PVP extends Battle {
+  constructor(
+    private _firstPlayer: Fighter,
+    private _secondPlayer: Fighter,
+  ) {
+    super(_firstPlayer);
+  }
 
+  public fight(): number {
+    while (this._firstPlayer.lifePoints > 0
+      && this._secondPlayer.lifePoints > 0) {
+      this._firstPlayer.attack(this._secondPlayer);
+      if (this._secondPlayer.lifePoints > 0) {
+        this._secondPlayer.attack(this._firstPlayer);
+      }
+    }
+    return super.fight();
+  }
 }
 
 export default PVP;
